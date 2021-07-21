8f0d5b15e2b3aad644989ae8cb7996bbeecf5222
Alin Cortel <alin.cortel@swaplanguage.com>
2021-07-21 18:10:04 +0200

    update appsettings with serilog sinks and new LoanConfig Section set language to 9 for all projects, 10 not supported yet Update startup to configure LoanConfiguration Update Program Configuration by including json config, serilog

--- BankChallenge/Program.cs
+++ BankChallenge/Program.cs
@@ -1,14 +1,16 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace BankChallenge
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Sinks.Email;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+
     public class Program
     {
         public static void Main(string[] args)
@@ -20,7 +22,60 @@ namespace BankChallenge
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .ConfigureAppConfiguration((context, config) =>
+                    {
+                        config
+                            .SetBasePath(context.HostingEnvironment.ContentRootPath)
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                            .AddEnvironmentVariables();
+
+                        if (args != null)
+                        {
+                            config.AddCommandLine(args);
+                        }
+                    })
+                        .ConfigureLogging((context, logging) =>
+                        {
+                            Serilog.ILogger? logger = Log.Logger = new LoggerConfiguration()
+                            .ReadFrom.Configuration(context.Configuration)
+                            .WriteTo.Email(GetEmailConnectionInfo(context.Configuration), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+                            .CreateLogger();
+                            logging.AddSerilog(logger);
+                        })
+
+                    .UseStartup<Startup>();
                 });
+
+
+        /// <summary>
+        /// this is a workaround i found to enable email sending for serilog and as well read serilog from json files without 
+        /// overriding each other
+        /// The documentation does not cover this scenario but i managed to find my way around it
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        private static EmailConnectionInfo? GetEmailConnectionInfo(IConfiguration configuration)
+        {
+            if (configuration.GetSection("Mail").Exists())
+            {
+                return new EmailConnectionInfo()
+                {
+                    EmailSubject = "Error in BankChallenge",
+                    EnableSsl = false,
+                    Port = int.Parse(configuration["Mail:Port"]),
+                    FromEmail = configuration["Mail:From"],
+                    ToEmail = configuration["Mail:To"],
+                    MailServer = configuration["Mail:Server"],
+                    NetworkCredentials = new NetworkCredential(configuration["Mail:User"], configuration["Mail:Password"]),
+
+                    // To bypass Certificate Validation that gives exception in production.
+                    //ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true
+                };
+            }
+
+            return null;
+        }
     }
 }
--- BankChallenge/Startup.cs
+++ BankChallenge/Startup.cs
@@ -1,19 +1,20 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace BankChallenge
 {
+    using BankChallenge.Common.Configuration;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.OpenApi.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -32,6 +33,9 @@ namespace BankChallenge
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BankChallenge", Version = "v1" });
             });
+
+            services
+               .Configure<LoanConfiguration>(options => Configuration.GetSection("LoanConfig").Bind(options));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
