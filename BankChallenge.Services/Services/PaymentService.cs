65158e91f402e5b57f96f5d5e06b8f0af9d31b7d
Alin Cortel <alin.cortel@swaplanguage.com>
2021-07-23 18:50:10 +0200

    add 2 more private method for calculation CalculateMonthlyPayment Done

--- BankChallenge.Services/Services/PaymentService.cs
+++ BankChallenge.Services/Services/PaymentService.cs
@@ -21,14 +21,41 @@
             throw new NotImplementedException();
         }
 
-        public Task<decimal> CalculateMonthlyPayment()
+        // A = Payment amount per period
+        // P = Initial principal(loan amount)
+        // r = Interest rate per period
+        // n = Total number of payments or periods
+        // The formula for calculating your monthly payment is:
+        // A = P { r(1 + r)n} / { (1 + r)n –1}
+        public async Task<decimal> CalculateMonthlyPayment(decimal totalLoan, decimal annualInterestRate, decimal totalPeriodYears)
         {
-            throw new NotImplementedException();
+            var ratePerPeriod = await CalculateInterestRatePerPeriod(annualInterestRate);
+            var totalPeriods = await CalculateTotalNumberOfPeriods(totalPeriodYears);
+
+            // this might not be the best approach. a proper way would be to extend Math.Pow to take decimals.
+            var rateAtPowerOfPeriods = (decimal)Math.Pow((double)(1 + ratePerPeriod), (double)totalPeriods);
+
+            // can't await here. i will await on method call.
+            return (totalLoan * ratePerPeriod * rateAtPowerOfPeriods) / (rateAtPowerOfPeriods - 1);
         }
 
         public async Task<Payment> Create()
         {
             throw new NotImplementedException();
+        }
+
+        // annual interest rate converted to percentage and divided by 12 months
+        private static async Task<decimal> CalculateInterestRatePerPeriod(decimal annualInterestRate)
+        {
+            // can't await here. i will await on metho call.
+            return (annualInterestRate / 100) / 12;
+        }
+
+        // the total number of periods represent the total months in which it is expected that the loan is paid
+        private static async Task<decimal> CalculateTotalNumberOfPeriods(decimal years)
+        {
+            // can't await here. i will await on metho call.
+            return years * 12;
         }
     }
 }
