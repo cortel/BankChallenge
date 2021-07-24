5697d444d2bb8f661b58f0993dab9f4f0eb38674
Alin Cortel <alin.cortel@swaplanguage.com>
2021-07-24 17:37:50 +0200

    update doc regards datacontext payment class

--- BankChallenge.DataContext/Entities/Payment.cs
+++ BankChallenge.DataContext/Entities/Payment.cs
@@ -1,8 +1,10 @@
 ﻿ namespace BankChallenge.DataContext.Entities
 {
     /// <summary>
-    /// This class can be used to be mapped with an ORM -EF , NHibernate
+    /// This class can be used to be mapped with an ORM -EF , NHibernate, or mini orm like dapper
     /// And afterwards a model can be created on top of it so we dont have to work with Entity directly.(light weight).
+    /// In the future, a cache database can be created to temporarily store last 1000 entries from rest endpoint
+    /// and then we can simply return previous results, without the need to calculate everything once more.
     /// </summary>
     public class Payment
     {
