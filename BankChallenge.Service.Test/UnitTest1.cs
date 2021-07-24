fe46a2c42e8325c8c00c8353810a7d0869527b65
Alin Cortel <alin.cortel@swaplanguage.com>
2021-07-24 09:45:26 +0200

    unit test environment fixed

---
+++ BankChallenge.Service.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankChallenge.Service.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- BankChallenge.Services.Test/PaymentServiceTest.cs
+++ BankChallenge.Services.Test/PaymentServiceTest.cs
@@ -7,7 +7,7 @@
     using Microsoft.Extensions.Options;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    public class PaymentServiceTest : BaseTest
+    public class PaymentServiceTest
     {
         private readonly PaymentService paymentService;
         private readonly IOptions<LoanConfiguration> loanConfig;
@@ -15,10 +15,10 @@
         public PaymentServiceTest()
         {
             // to compare if values are correctly passed from json
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+           var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-            loanConfig = Options.Create(new LoanConfiguration());
-            paymentService = new PaymentService(loanConfig);
+           loanConfig = Options.Create(new LoanConfiguration());
+           paymentService = new PaymentService(loanConfig);
         }
 
         [TestClass]
@@ -30,11 +30,6 @@
             [TestCategory("Create")]
             public void Can_Create_Payment()
             {
-                // Arrange
-
-                // Act
-
-                // Assert
             }
 
             [TestMethod]
@@ -43,10 +38,14 @@
             public void Can_Calculate_Monthly_Payment()
             {
                 // Arrange
+                var totalLoan = 500000;
+                var years = 10;
 
                 // Act
+                var result = paymentService.CalculateMonthlyPayment(totalLoan, years);
 
                 // Assert
+                Assert.AreEqual(5303.28, result);
             }
 
             [TestMethod]
